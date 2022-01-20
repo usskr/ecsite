748c908384724681381a602ba5a68f432cd2e7a4
usskr <rei.takagi731@gmail.com>
2022-01-20 09:24:33 +0000

    ヘッダー追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
-  
+
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+
+  devise_scope :customer do
+    root to: "public/registrations#new"
+  end
 end
